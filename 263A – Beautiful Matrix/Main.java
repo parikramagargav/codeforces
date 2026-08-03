342f6ce465cc3a0558174c54f8ab22e9c678160d
parikramagargav <parikramagargav@gmail.com>
2026-08-03 23:25:11 +0530

    Solved 263A – Beautiful Matrix

---
+++ 263A – Beautiful Matrix/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                int x = sc.nextInt();
+
+                if (x == 1) {
+                    System.out.println(Math.abs(i - 2) + Math.abs(j - 2));
+                    return;
+                }
+            }
+        }
+    }
+}
